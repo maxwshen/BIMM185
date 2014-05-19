b256c6f1145d8b7d05ddadb965e4ed16e7983d67
Max Shen <maxwshen@gmail.com>
2014-05-18 22:56:34 -0700

    Implemented simple grid approach

--- optimize.py
+++ optimize.py
@@ -34,7 +34,9 @@ def main():
   global mms_lowbound
   global mms_highbound
   global go_highbound
+  global go_lowbound
   global ge_highbound
+  global ge_lowbound
   ms = 1
   mms = -2
   go = -2
@@ -42,44 +44,65 @@ def main():
   mms_lowbound = -4.5
   mms_highbound = -0.6
   go_highbound = 0
+  go_lowbound = -4.5
   ge_highbound = 0
+  ge_lowbound = -3
 
 
   optimize(seq1, seq2)
 
 def optimize(seq1, seq2):
-  seed = [-1, -1, -1]
-  iterate = True
-  while iterate:
-    explore(seq1, seq2, seed, .1)
+  seeds = []
+  traversed = set()
+  gridsize = 0.5
+  for i in np.arange(mms_lowbound, mms_highbound, gridsize):
+    for j in np.arange(go_lowbound, go_highbound, gridsize):
+      for k in np.arange(ge_lowbound, ge_highbound, gridsize):
+        seeds.append([i, j, k])
+  print len(seeds), 'starting seeds at distance', gridsize
+  seeds = list(np.random.permutation(seeds))
 
-    iterate = False
+  best = defaultdict(list)
+  while len(seeds) > 0:
+    seed = seeds[0]
+    seeds = seeds[1:]
+    if tuple(seed) not in traversed:
+      traversed.add(tuple(seed))
+      bestdir, bestAcc = explore(seq1, seq2, seed, .1)
+      if bestdir is not None:
+        seeds.append(bestdir)
+      best[bestAcc].append(bestdir)
+      print best
 
   return
-
   # Brute force 
-  results = defaultdict(list)
-  for i in np.arange(-0.6, -4.5, -0.2):
-    mms = i
-    for j in np.arange(0, -4.5, -0.2):
-      go = j
-      for k in np.arange(0, -3, -0.2):
-        ge = k
-        # print ms, mms, go, ge
-        stats = locAL.external(seq1, seq2, ms, mms, go, ge)
-        if stats not in results:
-          results[stats] = [(ms, mms, go, ge)]
-        else:
-          results[stats].append((ms, mms, go, ge))
+  # results = defaultdict(list)
+  # for i in np.arange(-0.6, -4.5, -0.2):
+  #   mms = i
+  #   for j in np.arange(0, -4.5, -0.2):
+  #     go = j
+  #     for k in np.arange(0, -3, -0.2):
+  #       ge = k
+  #       # print ms, mms, go, ge
+  #       stats = locAL.external(seq1, seq2, ms, mms, go, ge)
+  #       if stats not in results:
+  #         results[stats] = [(ms, mms, go, ge)]
+  #       else:
+  #         results[stats].append((ms, mms, go, ge))
 
-  for i in results:
-    print float(i[1])/float(i[0]), i, ':',
-    for j in results[i]:
-      print '({0}, {1}, {2}, {3})'.format(round(j[0], 2), round(j[1], 2), round(j[2], 2), round(j[3], 2)),
-    print '\n',
+  # for i in results:
+  #   print float(i[1])/float(i[0]), i, ':',
+  #   for j in results[i]:
+  #     print '({0}, {1}, {2}, {3})'.format(round(j[0], 2), round(j[1], 2), round(j[2], 2), round(j[3], 2)),
+  #   print '\n',
 
 # Explores seed in all directions in 3D Space (27 possibilities) with 
+# _step size
+# Output:
+#   Returns a random seed of the highest accuracy 
 def explore(seq1, seq2, seed, step):
+  best = defaultdict(list)
+
   for i in [-step, 0, step]:
     for j in [-step, 0, step]:
       for k in [-step, 0, step]:
@@ -89,7 +112,20 @@ def explore(seq1, seq2, seed, step):
         if mms_lowbound < mms_t < mms_highbound and go_t < go_highbound and ge_t < ge_highbound:
           print mms_t, go_t, ge_t
           stats = locAL.external(seq1, seq2, ms, mms_t, go_t, ge_t)
-          print stats, float(stats[1]*100)/float(stats[0])
+          accuracy = float(stats[1]*100)/float(stats[0])
+          print stats, accuracy
+          best[accuracy].append((i, j, k))
+
+  # return best
+
+  # print max(best), best[max(best)]
+  # print best
+  if max(best) > 60:
+    direction = np.random.permutation(best[max(best)])[0]
+    bestdir = [seed[0] + direction[0], seed[1] + direction[1], seed[2] + direction[2]]
+    return bestdir, max(best)
+  else:
+    return None, max(best)
 
 
 # Initiates program and records total time
