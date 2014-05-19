66dbbf4abf4c05e10fc8b8c6d8f14f26a4647dd1
Max Shen <maxwshen@gmail.com>
2014-05-19 11:24:21 -0700

    Wrote simple graphing script

---
+++ graph.py
@@ -0,0 +1,22 @@
+# Run on Windows, not debruijn
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+  if len(sys.argv) != 2:
+    print 'Usage: python graph.py <input_file>'
+    sys.exit(0)
+  graph(sys.argv[1])
+
+def graph(inputfile):
+  with open(inputfile) as f:
+    lines = f.readlines()
+
+  plt.plot(lines)
+
+if __name__ == '__main__':
+  start = datetime.datetime.now()
+  print 'Start:', start
+  main()
+  end = datetime.datetime.now()
+  print '\nEnd:', end, '\nTotal:', end - start
